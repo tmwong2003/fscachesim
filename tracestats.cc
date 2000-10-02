01adf2a4540feba7bed65c3126ade5c16674a743
tmwong <tmwong>
2000-10-02 18:17:42 +0000

    Added tracestats program - works just like the old one I wrote, but now uses fscachesim functionality.
    CVS-ID: tracestats.cc 1.1

---
+++ tracestats.cc
@@ -0,0 +1,58 @@
+/*
+  RCS:          $Header: /afs/cs.cmu.edu/user/tmwong/pdl-62/Cvs/fscachesim/main.cc,v 1.3 2000/09/28 02:54:50 tmwong Exp $
+  Description:  Generate LRU and frequency trace stats using fscachesim
+                objects.
+  Author:       T.M. Wong <tmwong@cs.cmu.edu>
+*/
+
+#include <functional>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "BlockStoreInfinite.hh"
+#include "IORequest.hh"
+#include "IORequestGenerator.hh"
+#include "Node.hh"
+
+const int globalBlockSize = 4096;
+
+const int globalRecordsPerDot = 1000;
+
+class IORequestGeneratorLess:
+  public binary_function<IORequestGenerator *, IORequestGenerator *, bool> {
+public:
+  bool operator()(const IORequestGenerator *inGenL,
+		  const IORequestGenerator *inGenR) {
+    return (*inGenL < *inGenR);
+  };
+};
+
+int
+main(int argc, char *argv[])
+{
+  list<IORequestGenerator *> generators;
+  BlockStoreInfinite cache(0, globalBlockSize);
+  Node host(&cache, NULL);
+  int records = 0;
+
+  for (int i = 1; i < argc; i++) {
+    IORequestGenerator *generator = new IORequestGenerator(&host, argv[i]);
+
+    generators.push_back(generator);
+  }
+
+  bool requestProcessed;
+  do {
+    generators.sort(IORequestGeneratorLess());
+    requestProcessed = generators.front()->IORequestDown();
+
+    records++;
+    if (records % globalRecordsPerDot == 0) {
+      fprintf(stderr, ".");
+      fflush(stderr);
+    }
+  } while (requestProcessed);
+  fprintf(stderr, "\n");
+
+  host.statisticsShow();
+}
