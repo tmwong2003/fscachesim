36e1e6c169d234e19ce42e09fbdd69417abd18a5
tmwong <tmwong>
2001-06-30 22:54:06 +0000

    Changed LRU output to cache size in MB.
    CVS-ID: BlockStoreInfinite.cc 1.5

--- BlockStoreInfinite.cc
+++ BlockStoreInfinite.cc
@@ -1,5 +1,5 @@
 /*
-  RCS:          $Header: /afs/cs.cmu.edu/user/tmwong/pdl-62/Cvs/fscachesim/BlockStoreInfinite.cc,v 1.3 2000/10/24 19:54:41 tmwong Exp $
+  RCS:          $Header: /afs/cs.cmu.edu/user/tmwong/Cvs/fscachesim/BlockStoreInfinite.cc,v 1.4 2000/10/26 16:14:24 tmwong Exp $
   Description:  
   Author:       T.M. Wong <tmwong+@cs.cmu.edu>
 */
@@ -107,7 +107,10 @@ void
 BlockStoreInfinite::statisticsLRUShow() const
 {
   for (uint32MapConstIter i = LRUMap.begin(); i != LRUMap.end(); i++) {
-    printf("%d %d\n", i->first, i->second);
+    // Convert the x-axis to kilobytes instead of block size - the
+    // latter is not an intuitive measure of size.
+
+    printf("%d %d\n", (i->first * blockSize / (1024 * 1024)), i->second);
   }
   fflush(stdout);
 }
@@ -125,8 +128,11 @@ BlockStoreInfinite::statisticsLRUCumulShow() const
     cumulTotal += i->second;
   }
   for (uint32MapConstIter i = LRUMap.begin(); i != LRUMap.end(); i++) {
+    // Convert the x-axis to kilobytes instead of block size - the
+    // latter is not an intuitive measure of size.
+
     cumul += i->second;
-    printf("%d %4.3f\n", i->first, ((double)cumul / cumulTotal));
+    printf("%d %4.3f\n", (i->first * blockSize / (1024 * 1024)), ((double)cumul / cumulTotal));
   }
   fflush(stdout);
 }
